90c88118a208dd44bd4183740d8773d21afc8e1e
alcibiades g <alcibiades@iplgsc.com>
2024-10-08 12:41:23 -0500

    revirtiendo cambios en index_m

--- host_virtual_M/index/index_m.php
+++ host_virtual_M/index/index_m.php
@@ -10,13 +10,14 @@
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
   <link rel="stylesheet" href="../../main-global.css">
-  <!-- estilos ccs -->
-  <!-- Datatable -->
+  <!--estilos ccs-->
+  <!--Datatable-->
   <link rel="stylesheet" href="https://cdn.datatables.net/2.1.7/css/dataTables.dataTables.css">
+  <link rel="stylesheet" href="https://code.jquery.com/jquery-3.7.1.js">
   <link rel="stylesheet" href="https://cdn.datatables.net/2.1.7/css/dataTables.bootstrap4.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
   <link href="https://cdn.datatables.net/v/dt/jq-3.7.0/jszip-3.10.1/dt-2.1.7/b-3.1.2/b-html5-3.1.2/b-print-3.1.2/cr-2.0.4/date-1.5.4/fc-5.0.2/kt-2.12.1/r-3.0.3/rg-1.5.0/rr-1.5.0/sc-2.4.3/sb-1.8.0/sp-2.3.2/sl-2.1.0/sr-1.4.1/datatables.min.css" rel="stylesheet">
-  <!-- estilos ccs -->
+  <!--estilos ccs-->
 </head>
 
 <body style="background-image: url('../../host_virtual_TI/images/Motivo2.png'); overflow: visible">
@@ -71,35 +72,86 @@
       </div>
     </div>
   <?php
-  }
-  ?>
-
-  <div class="container">
-    <div class="row">
-      <div class="col-md-12">
-        <a href="crear_m.php" class="btn btn-success"><i class="bi bi-pen-fill"></i> Crear Ticket</a>
-        <hr>
-      </div>
+}
+?>
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+      <a href="crear_m.php"  class="btn btn-success "><i class="bi bi-pen-fill"></i> Crear Ticket</a>
+      <hr>
     </div>
   </div>
+</div>
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+      <h2 class="mt-3">Listado de ticket de Mantenimiento</h2>
+      <table class="table shadow-sm p-3 mb-5 bg-body-tertiary rounded table-striped">
+        <thead>
+          <tr>
+            <th>TID</th>
+            <th>Solicitante</th>
+            <th>correo</th>
+            <th>Departamento</th>
+            <th>Estado del problema</th>
+            <th>nivel de urgencia</th>
+            <th>Respuesta</th>
+            <th>Estado</th>
+            <th>fecha de creacion</th>
+            <th>ultima actualizacion</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php
+          if ($tickets && $sentencia->rowCount() > 0) {
+            foreach ($tickets as $fila) {
+              ?>
+              <tr>
+                <td class="text-break"><?php echo escapar($fila["id"]); ?></td>
+                <td class="text-break"><?php echo escapar($fila["nombrecompleto"]); ?></td>
+                <td class="text-break"><?php echo escapar($fila["correo"]); ?></td>
+                <td class="text-break"><?php echo escapar($fila["ubicacion"]); ?></td>
+                <td class="text-break"><?php echo escapar($fila["descripcion"]); ?></td>
+                <td class="text-break"><?php echo escapar($fila["urgencia"]); ?></td>
+                <td class="text-break"><?php echo escapar($fila["respuesta"]); ?></td>
+                <td class="text-break"><?php echo escapar($fila["estado"]); ?></td>
+                <td class="text-break"><?php echo escapar($fila["created_at"]); ?></td>
+                <td class="text-break"><?php echo escapar($fila["updated_at"]); ?></td>
+              </tr>
+              <?php
+            }
+          }
+          ?>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</div>
 
-  <div class="container">
+<?php include "../templates/footer.php"; ?>
+
+  <br>
+
+  <div class="tabla-container">
     <div class="row">
       <div class="col-md-12">
-        <h2 class="mt-3">Listado de ticket de Mantenimiento</h2>
-        <table id="tickMtable" class="table shadow-sm p-3 mb-5 bg-body-tertiary rounded table-striped">
+        <h2>Listado de ticket de Mantenimiento</h2>
+        <div class="col-md-12">
+          <a href="crear_m.php" class="btn btn-success "><i class="bi bi-pen-fill"></i> Crear Ticket</a>
+        </div>
+        <table id="tickMtable" class="table shadow p-3 mb-5 bg-body-tertiary rounded compact hover cell-border" style="background-color:#fff; width: 100%; margin-top: 1%;">
           <thead>
             <tr>
-              <th>TID</th>
-              <th>Solicitante</th>
-              <th>Correo</th>
-              <th>Departamento</th>
-              <th>Estado del problema</th>
-              <th>Nivel de urgencia</th>
-              <th>Respuesta</th>
-              <th>Estado</th>
-              <th>Fecha de creación</th>
-              <th>Última actualización</th>
+              <th class="border end">TID</th>
+              <th class="border end">Solicitante</th>
+              <!-- <th class="border end">Correo</th> -->
+              <th class="border end">Departamento</th>
+              <th class="border end">Descripción del problema</th>
+              <th class="border end">Nivel de urgencia</th>
+              <th class="border end">Respuesta</th>
+              <th class="border end">Estado</th>
+              <th class="border end">Fecha de creación</th>
+              <th class="border end">Ultima actualización</th>
             </tr>
           </thead>
           <tbody>
@@ -110,7 +162,7 @@
                 <tr>
                   <td class="text-break"><?php echo escapar($fila["id"]); ?></td>
                   <td class="text-break"><?php echo escapar($fila["nombrecompleto"]); ?></td>
-                  <td class="text-break"><?php echo escapar($fila["correo"]); ?></td>
+                  <!-- <td><?php #echo escapar($fila["correo"]); ?></td> -->
                   <td class="text-break"><?php echo escapar($fila["ubicacion"]); ?></td>
                   <td class="text-break"><?php echo escapar($fila["descripcion"]); ?></td>
                   <td class="text-break"><?php echo escapar($fila["urgencia"]); ?></td>
@@ -123,16 +175,33 @@
               }
             }
             ?>
-          </tbody> <!-- Aquí el cierre correcto de tbody -->
+          <tbody>
+          <tfoot>
+            <tr>
+              <th>TID</th>
+              <th>Solicitante</th>
+              <!-- <th>correo</th> -->
+              <th>Departamento</th>
+              <th>Descripción del problema</th>
+              <th>nivel de urgencia</th>
+              <th>Respuesta</th>
+              <th>Estado</th>
+              <th>fecha de creacion</th>
+              <th>ultima actualizacion</th>
+            </tr>
+          </tfoot>
         </table>
       </div>
     </div>
   </div>
-
+  <script src="../js/script.js"></script>
   <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
   <script src="https://cdn.datatables.net/2.1.7/js/dataTables.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
   <script src="https://cdn.datatables.net/2.1.7/js/dataTables.bootstrap5.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
+  <script src="https://cdn.datatables.net/v/dt/jq-3.7.0/jszip-3.10.1/dt-2.1.7/b-3.1.2/b-html5-3.1.2/b-print-3.1.2/cr-2.0.4/date-1.5.4/fc-5.0.2/kt-2.12.1/r-3.0.3/rg-1.5.0/rr-1.5.0/sc-2.4.3/sb-1.8.0/sp-2.3.2/sl-2.1.0/sr-1.4.1/datatables.min.js"></script>
 
   <script>
     new DataTable('#tickMtable', {
@@ -147,6 +216,7 @@
             placeholder: 'Busca un ticket'
           }
         },
+
         bottomEnd: {
           paging: {
             buttons: 3
@@ -162,10 +232,12 @@
             let column = this;
             let title = column.footer().textContent;
 
+            // Create input element
             let input = document.createElement('input');
             input.placeholder = title;
             column.footer().replaceChildren(input);
 
+            // Event listener for user input
             input.addEventListener('keyup', () => {
               if (column.search() !== this.value) {
                 column.search(input.value).draw();
