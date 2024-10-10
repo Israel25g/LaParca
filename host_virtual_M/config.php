b95ce968974fdb4478fb94429b7384c4228c2786
alcibiades g <alcibiades@iplgsc.com>
2024-10-10 12:49:31 -0500

    fix: archivo de conexion en el sistema de tickets

--- host_virtual_M/config.php
+++ host_virtual_M/config.php
@@ -2,10 +2,10 @@
 
 return [
   'db' => [
-  'host' => 'localhost',
-  'user' => 'root',
-  'pass' => '',
-  'name' => 'db_mainbase',
+'host' => 'localhost',
+'user' => 'u366386740_admin123',
+'pass' => '1plGr0up01*',
+'name' => 'u366386740_db_mainbase',
     'options' => [
       PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     ]
