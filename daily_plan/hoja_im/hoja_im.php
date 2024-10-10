fc25f0b9d41184b4cceb5c1f7f7c31a6d3edad93
alcibiades g <alcibiades@iplgsc.com>
2024-10-10 09:24:16 -0500

    fix: corrigiendo rutas para los dondos de pantalla en las hojas de datos en masa im,pk y ex

--- daily_plan/hoja_im/hoja_im.php
+++ daily_plan/hoja_im/hoja_im.php
@@ -11,7 +11,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/handsontable@11.0.0/dist/handsontable.full.min.css">
     <script src="https://cdn.jsdelivr.net/npm/handsontable@11.0.0/dist/handsontable.full.min.js"></script>
 </head>
-<body style="background-image:url('../images/Motivo2.png')!important;margin: 0;padding: 0; font-family:montserrat;">
+<body style="background-image:url('../../images/Motivo2.png')!important;margin: 0;padding: 0; font-family:montserrat;">
     <div class="form-table">
         <!-- Header -->
         <div class="header">
