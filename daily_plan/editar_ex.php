97f8f75bdf21c3414965f0be0cc1c2029e34c2ed
alcibiades g <alcibiades@iplgsc.com>
2024-10-10 14:32:50 -0500

    fix: ajustes de css en editar_ex.php

--- daily_plan/editar_ex.php
+++ daily_plan/editar_ex.php
@@ -161,7 +161,7 @@
       <div class="container mt-2">
         <div class="row">
           <div class="col-md-12">
-            <div class="alert alert-success" role="alert" style="margin-top: 490%; position: absolute">
+            <div class="alert alert-success" role="alert" style="margin-top: 490%;margin-left: 500%; position: absolute">
               <?= $resultado['mensaje'] ?>
             </div>
           </div>
@@ -176,7 +176,7 @@
             <h2 class="mt-4">Editando el campo  #<?= escapar($export['id'])?> de la tabla Export, del cliente <?= escapar($export['cliente'])?>.</h2>
             <a class="btn btn-success" href="../daily_plan/tabla_ex.php">Regresar a la tabla Export</a>
             <hr>
-          <form method="POST">
+          <form method="POST" style="margin-top: 50px;">
             <div class="form-group">
               <label for="pedidos_en_proceso">Nueva cantidad de pedidos en proceso (modificar solo de ser necesario).</label>
               <textarea name="pedidos_en_proceso" id="pedidos_en_proceso" rows="1" class="form-control" placeholder="Anterior cantidad de pedidos en proceso: <?= escapar($export['pedidos_en_proceso']) ?>"><?= escapar($export['pedidos_en_proceso']) ?></textarea>
