2c597caef95caaf23d238a404ee7c1d564df919e
alcibiades g <alcibiades@iplgsc.com>
2024-10-11 11:21:04 -0500

    fix: corrigiendo ruta de retorno en crear_eemp

--- host_virtual_EEMP/index/crear_eemp.php
+++ host_virtual_EEMP/index/crear_eemp.php
@@ -112,7 +112,7 @@ if (isset($_POST['submit'])) {
     <div class="row">
         <div class="col-md-12">
             <h2 class="mt-4"><a href="../../host_virtual_TI/index/index_ti.php"><i class="bi bi-caret-left-fill arrow-back"></i></a>Ingrese los datos para crear el Ticket</h2>
-            <a class="btn btn-success btn-lg" href="../index/index_ti.php">Volver al listado</a>
+            <a class="btn btn-success btn-lg" href="../index/index_eemp.php">Volver al listado</a>
             <hr>
             <form method="post">
                 <div class="form-group">
