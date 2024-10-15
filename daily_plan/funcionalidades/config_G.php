42127120bec4995d85fa8d0c139a765c2ef90fb6
alcibiades g <alcibiades@iplgsc.com>
2024-10-15 10:26:08 -0500

    hot fix:ajustes conexion de graficos

--- daily_plan/funcionalidades/config_G.php
+++ daily_plan/funcionalidades/config_G.php
@@ -1,7 +1,7 @@
 <?php
     $servername = "localhost";
-    $database = "u366386740_db_dailyplan";
-    $username = "u366386740_adminDP";
+    $database = "u366386740_db_mainbase";
+    $username = "u366386740_admin123";
     $password = "1plGr0up01*"; 
 
 // Crear la conexión
