2c07088c997472763cc33ac359cbe50f6985beac
alcibiades g <alcibiades@iplgsc.com>
2024-10-16 14:02:55 -0500

    ajustando los archivos de conexion a base de datos antes del merge a main

--- login/Conexion.php
+++ login/Conexion.php
@@ -1,7 +1,7 @@
 <?php
     $servername = "localhost";
-    $database = "u366386740_db_test_main";
-    $username = "u366386740_adminTestMain";
+    $database = "u366386740_db_mainbase";
+    $username = "u366386740_admin123";
     $password = "1plGr0up01*";
     // Create connection
     $conexion = new mysqli($servername, $username, $password, $database);
