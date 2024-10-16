f0764caa366e34ef35a7d42c3b23723185e01451
alcibiades g <alcibiades@iplgsc.com>
2024-10-16 12:51:16 -0500

    fix: botones de datatables

--- login/Conexion.php
+++ login/Conexion.php
@@ -1,8 +1,8 @@
 <?php
     $servername = "localhost";
-    $database = "u366386740_db_mainbase";
-    $username = "u366386740_admin123";
-    $password = "1plGr0up01*";
+    $database = "u366386740_db_test_main";
+    $username = "root";
+    $password = "";
     // Create connection
     $conexion = new mysqli($servername, $username, $password, $database);
     // Check connection
@@ -26,7 +26,7 @@
 
     <!-- ==============================LOCALHOST======================================
     $servername = "localhost";
-    $database = "db_mainbase";
+    $database = "u366386740_db_test_main";
     $username = "root";
     $password = "";
     // Create connection
