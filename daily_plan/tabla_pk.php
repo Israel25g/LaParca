314baf8ba48a2ab06a08693b19f3598bbf3100e6
alcibiades g <alcibiades@iplgsc.com>
2024-10-17 08:22:57 -0500

    ajustando impresion en la tabla pk

--- daily_plan/tabla_pk.php
+++ daily_plan/tabla_pk.php
@@ -185,35 +185,36 @@ include("../apertura_sesion.php");
                       extend: 'copy',
                       text: 'Copiar',
                       exportOptions: {
-                        columns: ':not(:last-child)'  // Excluir la última columna
+                        columns: [0, 1, 2,3,4,5,6,7,8,9] 
+
                       }
                     },
                     {
                       extend: 'csv',
                       text: 'CSV',
                       exportOptions: {
-                        columns: ':not(:last-child)'  // Excluir la última columna
+                        columns: [0, 1, 2,3,4,5,6,7,8,9]
                       }
                     },
                     {
                       extend: 'excel',
                       text: 'Excel',
                       exportOptions: {
-                        columns: ':not(:last-child)'  // Excluir la última columna
+                        columns: [0, 1, 2,3,4,5,6,7,8,9]
                       }
                     },
                     {
                       extend: 'pdf',
                       text: 'PDF',
                       exportOptions: {
-                        columns: ':not(:last-child)'  // Excluir la última columna
+                        columns: [0, 1, 2,3,4,5,6,7,8,9]
                       }
                     },
                     {
                       extend: 'print',
                       text: 'Imprimir',
                       exportOptions: {
-                        columns: ':not(:last-child)'  // Excluir la última columna
+                        columns: [0, 1, 2,3,4,5,6,7,8,9]
                       }
                     }
                   ],
