5ace4d301b90958835213c73ba49787ddbac380f
alcibiades g <alcibiades@iplgsc.com>
2024-10-22 14:44:10 -0500

    añadiendo datatables a las tablas de los admin de soporte tecnologico

--- host_virtual_TI/indexAdmin/indexAdmin_ti.php
+++ host_virtual_TI/indexAdmin/indexAdmin_ti.php
@@ -56,7 +56,7 @@
       <div class="row">
         <div class="col-md-12">
           <h2 class="mt-3"><a href="../../helpdesk.php"><i class="bi bi-caret-left-fill arrow-back"></i></a>Listado de tickets de TI</h2>
-          <table class="table shadow-sm p-3 mb-5 bg-body-tertiary table-striped" style="--bs-border-opacity: .5;">
+          <table id="admin_ti" class="table shadow-sm p-3 mb-5 bg-body-tertiary table-striped" style="--bs-border-opacity: .5;">
             <thead>
             <tr>
                 <th>TID</th>
@@ -124,6 +124,99 @@
         </div>
       </div>
     </div>
+    <script>
+      $(document).ready(function() {
+        new DataTable('#admin_ti', {
+          paging: false,
+          scrollCollapse: true,
+          scrollY: '350px',
+          scrollX: '1700px',
+
+          initComplete: function() {
+            this.api()
+              .columns()
+              .every(function() {
+                let column = this;
+                let title = column.footer().textContent;
+
+                // Create input element
+                let input = document.createElement('input');
+                input.placeholder = title;
+                column.footer().replaceChildren(input);
+
+                // Event listener for user input
+                input.addEventListener('keyup', () => {
+                  if (column.search() !== this.value) {
+                    column.search(input.value).draw();
+                  }
+                });
+              });
+          },
+
+          buttons: [
+                    {
+                      extend: 'copy',
+                      text: 'Copiar',
+                      exportOptions: {
+                        columns: [0, 1, 2,3,4,5,6,7,8] 
+
+                      }
+                    },
+                    {
+                      extend: 'csv',
+                      text: 'CSV',
+                      exportOptions: {
+                        columns: [0, 1, 2,3,4,5,6,7,8]
+                      }
+                    },
+                    {
+                      extend: 'excel',
+                      text: 'Excel',
+                      exportOptions: {
+                        columns: [0, 1, 2,3,4,5,6,7,8]
+                      }
+                    },
+                    {
+                      extend: 'pdf',
+                      text: 'PDF',
+                      exportOptions: {
+                        columns: [0, 1, 2,3,4,5,6,7,8]
+                      }
+                    },
+                    {
+                      extend: 'print',
+                      text: 'Imprimir',
+                      exportOptions: {
+                        columns: [0, 1, 2,3,4,5,6,7,8]
+                      }
+                    }
+                  ],
+                  dom: 'Bfrtip', // Asegura que los botones aparezcan en el lugar correcto
+          info: false,
+          language: {
+            "lengthMenu": "Mostrar _MENU_ registros por página",
+            "zeroRecords": "No se encontraron resultados",
+            "info": "Mostrando página _PAGE_ de _PAGES_",
+            "infoEmpty": "No hay registros disponibles",
+            "infoFiltered": "(filtrado de _MAX_ registros totales)",
+            "search": "Buscar:",
+            "paginate": {
+              "first": "<◀",
+              "last": "▶> ",
+              "next": "▶",
+              "previous": "◀"
+            },
+            "buttons": {
+              "copy": "Copiar",
+              "csv": "CSV",
+              "excel": "Excel",
+              "pdf": "PDF",
+              "print": "Imprimir"
+            }
+          }
+        });
+      });
+    </script>
     <?php include "../templates/footer.php"; ?>
   </body>
 </html>
