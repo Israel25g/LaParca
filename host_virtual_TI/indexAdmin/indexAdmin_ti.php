5b6b8e463c0b7fc86fe24b455fc42f935ad9e55f
alcibiades g <alcibiades@iplgsc.com>
2024-10-22 15:22:47 -0500

    ajustando ubicacion de la tabla

--- host_virtual_TI/indexAdmin/indexAdmin_ti.php
+++ host_virtual_TI/indexAdmin/indexAdmin_ti.php
@@ -74,7 +74,7 @@
 
 
 
-    <div class="container">
+    <div class="container" style="margin-top: 400px !important;">
       <div class="row">
         <div class="col-md-12">
           <h2 class="mt-3"><a href="../../helpdesk.php"><i class="bi bi-caret-left-fill arrow-back"></i></a>Listado de tickets de TI</h2>
