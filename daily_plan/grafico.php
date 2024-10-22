272fe095a485008443d1800fc5901f2d76880e11
alcibiades g <alcibiades@iplgsc.com>
2024-10-22 07:55:22 -0500

    revirtiendo ultimo cambio

--- daily_plan/grafico.php
+++ daily_plan/grafico.php
@@ -484,7 +484,7 @@
             tooltip: {
                 trigger: 'axis',
                 axisPointer: {
-                    type: 'line' // 'shadow' as default; can also be 'line' or 'shadow'
+                    type: 'shadow' // 'shadow' as default; can also be 'line' or 'shadow'
                 }
             },
             legend: {},
