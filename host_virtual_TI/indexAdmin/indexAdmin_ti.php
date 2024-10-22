ac1636d1189eef374ee0201a0a51499c7352f3db
alcibiades g <alcibiades@iplgsc.com>
2024-10-22 14:57:18 -0500

    añadiendo reloj y demas funcionalidades

--- host_virtual_TI/indexAdmin/indexAdmin_ti.php
+++ host_virtual_TI/indexAdmin/indexAdmin_ti.php
@@ -143,6 +143,7 @@
         </div>
       </div>
     </div>
+    <script src="../../host_virtual_TI/js/script.js"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
   <script src="https://cdn.datatables.net/2.1.7/js/dataTables.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
