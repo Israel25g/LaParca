7224d91b1906b393a7646c2f5e692e6d117b847d
alcibiades g <alcibiades@iplgsc.com>
2024-10-23 16:23:23 -0500

    corrigiendo credenciales de daily_plan

--- daily_plan/funcionalidades/config_DP.php
+++ daily_plan/funcionalidades/config_DP.php
@@ -3,9 +3,9 @@
 return [
   'db' => [
     'host' => 'localhost',
-    'user' => 'u366386740_adminDP',
+    'user' => 'u366386740_adminTestDP',
     'pass' => '1plGr0up01*',
-    'name' => 'u366386740_db_dailyplan',
+    'name' => 'u366386740_db_test_dp',
     'options' => [
       PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     ]
@@ -32,9 +32,9 @@ return [
 // return [
 //   'db' => [
 //     'host' => 'localhost',
-//     'user' => 'u366386740_adminTestMain',
+//     'user' => 'u366386740_adminTestDP',
 //     'pass' => '1plGr0up01*',
-//     'name' => 'u366386740_db_test_main',
+//     'name' => 'u366386740_db_test_dp',
 //     'options' => [
 //       PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 //     ]
