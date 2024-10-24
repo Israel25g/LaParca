05727fa1ece726285173f549ebf76cff820d244a
Israel <israel@iplgsc.com>
2024-10-24 16:26:45 -0500

    Archivos de configuración para main

--- daily_plan/funcionalidades/config_DP.php
+++ daily_plan/funcionalidades/config_DP.php
@@ -3,9 +3,9 @@
 return [
   'db' => [
     'host' => 'localhost',
-    'user' => 'u366386740_adminTestDP',
+    'user' => 'u366386740_adminDP',
     'pass' => '1plGr0up01*',
-    'name' => 'u366386740_db_test_dp',
+    'name' => 'u366386740_db_dailyplan',
     'options' => [
       PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     ]
