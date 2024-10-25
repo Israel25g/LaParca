f89816f4e05da371872644f19a1cd9b64993756e
alcibiades g <alcibiades@iplgsc.com>
2024-10-25 09:28:43 -0500

    rolback

--- host_virtual_TI/js/script.js
+++ host_virtual_TI/js/script.js
@@ -1,30 +1,30 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     function updateDateTime() {
-//         const dateElement = document.getElementById("fecha-actual");
-//         const timeElement = document.getElementById("hora-actual");
-//         const now = new Date();
+document.addEventListener("DOMContentLoaded", function() {
+    function updateDateTime() {
+        const dateElement = document.getElementById("fecha-actual");
+        const timeElement = document.getElementById("hora-actual");
+        const now = new Date();
 
-//         const dateOptions = { 
-//             weekday: 'long', 
-//             year: 'numeric', 
-//             month: 'long', 
-//             day: 'numeric' 
-//         };
+        const dateOptions = { 
+            weekday: 'long', 
+            year: 'numeric', 
+            month: 'long', 
+            day: 'numeric' 
+        };
         
-//         const formattedDate = now.toLocaleDateString('es-ES', dateOptions);
+        const formattedDate = now.toLocaleDateString('es-ES', dateOptions);
 
-//         const hours = now.getHours().toString().padStart(2, '0');
-//         const minutes = now.getMinutes().toString().padStart(2, '0');
-//         const seconds = now.getSeconds().toString().padStart(2, '0');
-//         const formattedTime = `${hours}:${minutes}:${seconds}`;
+        const hours = now.getHours().toString().padStart(2, '0');
+        const minutes = now.getMinutes().toString().padStart(2, '0');
+        const seconds = now.getSeconds().toString().padStart(2, '0');
+        const formattedTime = `${hours}:${minutes}:${seconds}`;
 
-//         dateElement.textContent = formattedDate;
-//         timeElement.textContent = formattedTime;
-//     }
+        dateElement.textContent = formattedDate;
+        timeElement.textContent = formattedTime;
+    }
 
-//     updateDateTime();
-//     setInterval(updateDateTime, 1000);
-// });
+    updateDateTime();
+    setInterval(updateDateTime, 1000);
+});
 
 
 // temporizador
