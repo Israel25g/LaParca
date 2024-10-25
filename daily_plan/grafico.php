34c40dd8aa240aa4ac43ce025aabb3046317db5a
alcibiades g <alcibiades@iplgsc.com>
2024-10-25 10:31:08 -0500

    cambiando cosas

--- daily_plan/grafico.php
+++ daily_plan/grafico.php
@@ -53,7 +53,7 @@ try {
     <link rel="stylesheet" href="../estilos.css">
     <link rel="shortcut icon" href="../images/ICO.png">
     <!-- Incluir ECharts desde el CDN -->
-    <script src="https://cdn.jsdelivr.net/npm/echarts@5.3.0/dist/echarts.min.js"></script>
+   
 
 
     <style>
@@ -507,6 +507,6 @@ try {
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="../host_virtual_TI/js/script.js"></script>
-
+    <script src="https://cdn.jsdelivr.net/npm/echarts@5.3.0/dist/echarts.min.js"></script>
 </body>
 </html>
