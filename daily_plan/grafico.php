6e04ea1af30debb6699a38b08375808f4dc8c524
alcibiades g <alcibiades@iplgsc.com>
2024-10-25 13:53:31 -0500

    cambios

--- daily_plan/grafico.php
+++ daily_plan/grafico.php
@@ -202,30 +202,7 @@
                   <div class="col-md-3"  style=" width: 700px; height: 60%; margin-left: 250px">
                     <h2 class="mt-3" style="margin-bottom: 10px; font-size:30px; margin-left: 25% !important">Export</h2>
                     <div>
-                    <table id="tablaExport" class="  display table shadow p-3 mb-5 bg-body-info rounded table-striped border" style=" margin-left: 25% !important">
-                      <thead>
-                        <tr style="font-family: montserrat; font-size: 15px">
-                          <th class="border end" style="background-color: #dc3545">OID</th>
-                          <th class="border end" style="background-color: #dc3545">Cliente</th>
-                          <th class="border end" style="background-color: #dc3545">Vehiculo</th>
-                          <th class="border end" style="background-color: #dc3545">Pedidos en proceso</th>
-                          <th class="border end" style="background-color: #dc3545">Pedidos despachados</th>
-                        </tr>
-                      </thea class="table-container ">
-                      <tbody>
-                        <?php if ($export && $sentencia_e->rowCount() > 0): ?>
-                          <?php foreach ($export as $fila): ?>
-                            <tr style="font-family: montserrat; font-size: 14px">
-                              <td class="border end"><?php echo escapar($fila["aid_oid"]); ?></td>
-                              <td class="border end"><?php echo escapar($fila["cliente"]); ?></td>
-                              <td class="border end"><?php echo escapar($fila["vehiculo"]); ?></td>
-                              <td class="border end"><?php echo escapar($fila["pedidos_en_proceso"]); ?></td>
-                              <td class="border end"><?php echo escapar($fila["pedidos_despachados"]); ?></td>
-                            </tr>
-                          <?php endforeach; ?>
-                        <?php endif; ?>
-                      </tbody>
-                    </table>
+
                   </div>
                 </div>
               </div>
@@ -240,28 +217,7 @@
                           <div class="row">
                             <div class="col-md-3 " style=" width: 700px; height: 60%; margin-left: 250px">
                               <h2 class="mt-3" style="margin-bottom: 10px; font-size:30px; margin-left: 25% !important">Import</h2>
-                              <table id="tablaImport" class="   display table shadow p-3 mb-5 bg-body-info rounded table-striped border" style=" margin-left: 25% !important">
-                          <thead>
-                            <tr  style="font-family: montserrat; font-size: 15px">
-                              <th class="border end" style="background-color: #0dcaf0">AID</th>
-                              <th class="border end" style="background-color: #0dcaf0">Cliente</th>
-                              <th class="border end" style="background-color: #0dcaf0">Contenedores recibidos</th>
-                              <th class="border end" style="background-color: #0dcaf0">Contenedores cerrados</th>
-                            </tr>
-                          </thead>
-                          <tbody>
-                              <?php if ($import && $sentencia_i->rowCount() > 0): ?>
-                                    <?php foreach ($import as $fila): ?>
-                                      <tr style="font-family: montserrat; font-size: 14px">
-                                        <td class="border end"><?php echo escapar($fila["aid_oid"]); ?></td>
-                                        <td class="border end"><?php echo escapar($fila["cliente"]); ?></td>
-                                        <td class="border end"><?php echo escapar($fila["pedidos_en_proceso"]); ?></td>
-                                        <td class="border end"><?php echo escapar($fila["pedidos_despachados"]); ?></td>
-                                      </tr>
-                                    <?php endforeach; ?>
-                                  <?php endif; ?>
-                          </tbody>
-                        </table>
+
 
                             </div>
                           </div>
@@ -278,34 +234,7 @@
                         <h2 class="mt-2" style="margin-bottom: 10px; font-size:30px ; margin-left: 25% !important">Picking</h2>
                                                       <!-- Tabla 1 -->
                         <div  id="tablapicking" class=" display table shadow p-3 mb-5 bg-body-info rounded table-striped border"  style="  margin-left: 25% !important">
-                            <table>
-                            <thead>
-                                  <tr style="font-family: montserrat; font-size: 14px">
-                                    <th class="border end" style="background-color: #ffc107">OID</th>
-                                    <th class="border end" style="background-color: #ffc107">Cliente</th>
-                                    <th class="border end" style="background-color: #ffc107">Prioridad de picking</th>
-                                    <th class="border end" style="background-color: #ffc107">Unidades por pickear</th>
-                                    <th class="border end" style="background-color: #ffc107">Unidades pickeadas</th>
-                                    <th class="border end" style="background-color: #ffc107">Porcentaje de avance</th>
-                                    <th class="border end" style="background-color: #ffc107">Fecha de requerido</th>
-                                  </tr>
-                                </thead>
-                                <tbody>
-                                <?php if ($picking && $sentencia_pk->rowCount() > 0): ?>
-                                    <?php foreach ($picking as $fila): ?>
-                                      <tr>
-                                        <td class="border end"><?php echo escapar($fila["aid_oid"]); ?></td>
-                                        <td class="border end"><?php echo escapar($fila["cliente"]); ?></td>
-                                        <td class="border end"><?php echo escapar($fila["vacio_lleno"]); ?></td>
-                                        <td class="border end"><?php echo escapar($fila["pedidos_en_proceso"]); ?></td>
-                                        <td class="border end"><?php echo escapar($fila["pedidos_despachados"]); ?></td>
-                                        <td class="border end"><?php echo escapar($fila["division_dp"])*100.00; ?>%</td>
-                                        <td class="border end"><?php echo escapar($fila["fecha_objetivo"]); ?></td>
-                                      </tr>
-                                    <?php endforeach; ?>
-                                  <?php endif; ?>
-                                </tbody>
-                            </table>
+
                         </div>
                       </div>
                     </div>
@@ -347,11 +276,11 @@
     </div>
   </div>
   <button class="carousel-control-prev btn-primary" type="button" data-bs-target="#carouselExampleSlidesOnly" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="carousel-control-prev-icon visually-hidden" aria-hidden="true"></span>
     <span class="visually-hidden">Previous</span>
   </button>
   <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleSlidesOnly" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="carousel-control-next-icon visually-hidden" aria-hidden="true"></span>
     <span class="visually-hidden">Next</span>
   </button>
 </div>
