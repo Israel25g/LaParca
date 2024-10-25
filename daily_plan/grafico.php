5abc9d90bcd60827e7a2819efab68ccd5ca1e33a
alcibiades g <alcibiades@iplgsc.com>
2024-10-25 13:50:59 -0500

    ajsutando z-index

--- daily_plan/grafico.php
+++ daily_plan/grafico.php
@@ -347,11 +347,11 @@
     </div>
   </div>
   <button class="carousel-control-prev btn-primary" type="button" data-bs-target="#carouselExampleSlidesOnly" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="false"></span>
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Previous</span>
   </button>
   <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleSlidesOnly" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="false"></span>
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Next</span>
   </button>
 </div>
