9d4c6f098df2dc4d9f5fd493305444329b74cad3
alcibiades g <alcibiades@iplgsc.com>
2024-10-28 10:58:27 -0500

    ajustando estilos en las tablas de datos

--- daily_plan/grafico.php
+++ daily_plan/grafico.php
@@ -193,8 +193,8 @@
             <div class="bloquess"style=";display: grid;grid-template-columns: auto auto;gap: 10px; margin-left: 10% !important;  margin-top: 10% !important">
 
             <div class="bloquee border border-5 border-danger" id="import"  style="position: relative;width: 800px; height: 400px;border-radius: 15px; overflow: hidden;box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
-            <div class="col-md-6" >
-            <div class="container" >
+            <div class="col-md-9" >
+            <div class="container"  >
                 <h2>Export</h2>
                 <table id="clientes-table_TEX" border="1" class="table table-danger table-striped" style="width: 70%;">
                   <thead class="bg-danger">
@@ -212,7 +212,7 @@
             </div>
             </div>
             <div class="bloquee border border-5 border-info" id="import"  style="position: relative;width: 800px; height: 400px;border-radius: 15px; overflow: hidden;box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
-            <div class="col-md-6" >
+            <div class="col-md-9" >
               <div class="container" >
                 <h2>Import</h2>
                 <table id="clientes-table_TIM" border="1" class="table table-info table-striped" style="width: 70%;">
@@ -231,7 +231,7 @@
             </div>
 
             <div class="bloquee border border-5 border-warning" id="import"  style="position: relative;width: 800px; height: 400px;border-radius: 15px; overflow: hidden;box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
-            <div class="col-md-6" >
+            <div class="col-md-9" >
               <div class="container" >
                 <h2>Picking</h2>
                 <table id="clientes-table_TPK" border="1" class="table table-warning table-striped" >
@@ -254,7 +254,7 @@
 
 <!-- porcentaje tablas -->
               <div class="bloquee" id="porcentaje" style="position: relative;width: 200%; height: 400px;border-radius: 15px; overflow: hidden;" >
-                <div class="col-md-6 " >
+                <div class="col-md-9 " >
                     <p class="titulo_gauge" style="font-family: montserrat; font-size:200%; font-weight: bold;">Porcentaje de cumplimiento</p>
                     <div id="grafico-gauge_d" style="width: 900; height: 450px;margin-top:-50px;margin-left:50px"></div>
                 </div>
