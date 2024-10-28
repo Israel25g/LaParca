65917c945e56ffa73c6dbf94ff006d3160ec8934
alcibiades g <alcibiades@iplgsc.com>
2024-10-28 16:31:12 -0500

    modificando distancias

--- daily_plan/grafico.php
+++ daily_plan/grafico.php
@@ -201,9 +201,9 @@
             </div>
             </div>
             <div class="bloquee border border-5 border-dark" id="porcentaje" style="position: relative;width: 800; height: 450px;border-radius: 15px; overflow: hidden;" >
-                <div style="position: relative; width: 800px; height: 800px; padding: 10px; margin-left: -80px; margin-top: -100px; border-width: 10px;">
+                <div style="position: relative; width: 800px; height: 800px; padding: 10px; margin-left: -80px;  border-width: 10px;">
                     <p class="titulo_gauge" style="font-family: montserrat; font-size:200%; font-weight: bold;">Porcentaje de cumplimiento</p>
-                    <div id="grafico-gauge_d" style="position: relative; width: 900px; height: 450px; padding: 10px; margin-top: -100px; border-width: 10px;"></div>
+                    <div id="grafico-gauge_d" style="position: relative; width: 900px; height: 450px; padding: 10px;  border-width: 10px;"></div>
                 </div>
             </div>
               </div>
