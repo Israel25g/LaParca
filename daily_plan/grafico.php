37067b918ffde3db83c073e848574859330bb039
alcibiades g <alcibiades@iplgsc.com>
2024-10-28 13:21:50 -0500

    realizando ajustes en la estetica general

--- daily_plan/grafico.php
+++ daily_plan/grafico.php
@@ -149,7 +149,7 @@
             <div class="bloquee border border-5 border-danger" id="import"  style="position: relative;width: 800px; height: 400px;border-radius: 15px; overflow: hidden;box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
             <h2 style="margin-left:45%">Export</h2>
             <div class="container">
-                <table id="clientes-table_TEX"  class="table table-danger table-striped" style="width: 80%;" >
+                <table id="clientes-table_TEX"  class="table table-danger table-striped" style="width: 800px;" >
                   <thead class="bg-danger">
                     <tr>
                       <th class="text-bg-danger">OID</th>
@@ -166,7 +166,7 @@
             <div class="bloquee border border-5 border-info" id="import"  style="position: relative;width: 800px; height: 400px;border-radius: 15px; overflow: hidden;box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
               <h2 style="margin-left:45%">Import</h2>
               <div class="container">
-                <table id="clientes-table_TIM"  class="table table-info table-striped" style="width: 80%;">
+                <table id="clientes-table_TIM"  class="table table-info table-striped" style="width: 800px;">
                   <thead class="bg-info">
                     <tr>
                       <th class="text-bg-info">AID</th>
