5ea9421b8eb9e64311a538de8cda0bf3a1d102bc
alcibiades g <alcibiades@iplgsc.com>
2024-10-28 09:50:12 -0500

    ajustano estilos de tablas de datos

--- daily_plan/grafico.php
+++ daily_plan/grafico.php
@@ -189,7 +189,7 @@
     </div>
 
             <div class="carousel-item" data-bs-interval="15000">
-            <div class="container" style="margin-top: 0%">
+            <div class="container" style="margin-top: 20%">
             <div class="bloquess"style=";display: grid;grid-template-columns: auto auto;gap: 10px; margin-left: -10% !important;  margin-top: 0% !important">
 
             <div class="bloquee border border-5 border-danger" id="import"  style="position: relative;width: 800px; height: 300px;border-radius: 15px; overflow: hidden;box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
