51329d61eb01aa4948568ffa99294f5bdc14ccd3
alcibiades g <alcibiades@iplgsc.com>
2024-10-28 09:42:45 -0500

    bugfix: corrigiendo respuesta de API-TIM

--- daily_plan/get_data_TIM.php
+++ daily_plan/get_data_TIM.php
@@ -16,7 +16,6 @@ $options = [
 try {
     $pdo = new PDO($dsn, $user, $pass, $options);
     // Verificar conexión exitosa
-    echo json_encode("");
 } catch (\PDOException $e) {
     echo json_encode(["error" => "Error de conexión: " . $e->getMessage()]);
     exit;
