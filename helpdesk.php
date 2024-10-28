2bce28ce1f2bceb53af1bdf85d7fc8573d5b8bd4
alcibiades g <alcibiades@iplgsc.com>
2024-10-28 12:17:16 -0500

    retirando temporizadores, ajustando estilos en la tablas

--- helpdesk.php
+++ helpdesk.php
@@ -130,7 +130,7 @@ include("apertura_sesion.php")
     <script src="host_virtual_TI/js/script.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/toastr@2.1.4/toastr.min.js"></script>
-    <script>
+    <!-- <script>
         // temporizador
 
         let registrarInactividad = function() {
@@ -157,7 +157,7 @@ include("apertura_sesion.php")
         }
 
         registrarInactividad();
-    </script>
+    </script> -->
 </body>
 
 </html>
