ee1c22d514f6de4a43c25200a7de4013e8a89bf0
alcibiades g <alcibiades@iplgsc.com>
2024-10-28 10:24:48 -0500

    rollback

--- daily_plan/grafico.php
+++ daily_plan/grafico.php
@@ -189,10 +189,12 @@
     </div>
 
             <div class="carousel-item" data-bs-interval="15000">
+            <div class="container" style="margin-top: 20%">
+            <div class="bloquess"style=";display: grid;grid-template-columns: auto auto;gap: 10px; margin-left: 10% !important;  margin-top: 10% !important">
 
-              <div class="container text-center">
-              <div class="row align-items-start">
-                <div class="col">
+            <div class="bloquee border border-5 border-danger" id="import"  style="position: relative;width: 800px; height: 400px;border-radius: 15px; overflow: hidden;box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
+            <div class="col-md-9" >
+            <div class="container" >
                 <h2>Export</h2>
                 <table id="clientes-table_TEX" border="1" class="table table-danger table-striped" style="width: 70%;">
                   <thead class="bg-danger">
@@ -206,8 +208,12 @@
                   </thead>
                   <tbody></tbody>
                 </table>
-                </div>
-                <div class="col">
+              </div>
+            </div>
+            </div>
+            <div class="bloquee border border-5 border-info" id="import"  style="position: relative;width: 800px; height: 400px;border-radius: 15px; overflow: hidden;box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
+            <div class="col-md-9" >
+              <div class="container" >
                 <h2>Import</h2>
                 <table id="clientes-table_TIM" border="1" class="table table-info table-striped" style="width: 70%;">
                   <thead class="bg-info">
@@ -220,8 +226,13 @@
                   </thead>
                   <tbody></tbody>
                 </table>
-                </div>
-                <div class="col">
+              </div>
+            </div>
+            </div>
+
+            <div class="bloquee border border-5 border-warning" id="import"  style="position: relative;width: 800px; height: 400px;border-radius: 15px; overflow: hidden;box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
+            <div class="col-md-12 " >
+              <div class="container" >
                 <h2>Picking</h2>
                 <table id="clientes-table_TPK" border="1" class="table table-warning table-striped" >
                   <thead class="bg-warning">
@@ -236,8 +247,19 @@
                   </thead>
                   <tbody></tbody>
                 </table>
-                </div>
               </div>
+            </div>
+            </div>
+
+
+<!-- porcentaje tablas -->
+              <div class="bloquee" id="porcentaje" style="position: relative;width: 200%; height: 400px;border-radius: 15px; overflow: hidden;" >
+                <div class="col-md-6 " >
+                    <p class="titulo_gauge" style="font-family: montserrat; font-size:200%; font-weight: bold;">Porcentaje de cumplimiento</p>
+                    <div id="grafico-gauge_d" style="width: 900; height: 450px;margin-top:-50px;margin-left:50px"></div>
+                </div>
+            </div>
+            </div>
             </div>
 
     </div>
