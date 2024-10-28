e8b2cce02eb0ca914c65fa74c2b3fdcb388b4413
alcibiades g <alcibiades@iplgsc.com>
2024-10-28 16:49:28 -0500

    ajustando alturas

--- daily_plan/grafico.php
+++ daily_plan/grafico.php
@@ -143,7 +143,7 @@
     </div>
 
             <div class="carousel-item" data-bs-interval="15000">
-            <div class="container" style="margin-top: 8%">
+            <div class="container" >
             <div class="bloquess"style=";display: grid;grid-template-columns: auto auto;gap: 5px">
 
             <div class="bloquee border border-5 border-danger" id="import"  style="position: relative;width: 800px; height: 400px;border-radius: 15px; overflow: hidden;box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
