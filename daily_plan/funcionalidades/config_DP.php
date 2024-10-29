c7f5643f7629f4fa488d0379aec5dfaf58e144b8
alcibiades g <alcibiades@iplgsc.com>
2024-10-29 07:44:32 -0500

    hotfix:conexion a base de datos

--- daily_plan/funcionalidades/config_DP.php
+++ daily_plan/funcionalidades/config_DP.php
@@ -1,1 +1,13 @@
 <?php
+
+return [
+  'db' => [
+    'host' => 'localhost',
+    'user' => 'u366386740_adminDP',
+    'pass' => '1plGr0up01*',
+    'name' => 'u366386740_db_dailyplan',
+    'options' => [
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]
+  ]
+];
