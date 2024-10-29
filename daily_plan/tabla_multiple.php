8671a8fc0de4fc54d9b109077f52c38006d21a75
alcibiades g <alcibiades@iplgsc.com>
2024-10-29 10:23:15 -0500

    fix: estilo de header

--- daily_plan/tabla_multiple.php
+++ daily_plan/tabla_multiple.php
@@ -73,7 +73,7 @@ try {
 <div class="container-lg" style="margin-top: 150px;">
 
     <!-- Header -->
-    <div class="header">
+    <div class="header-error">
     <div class="logo-container">
       <a href="https://iplgsc.com" target="_blank"><img class="logo" src="../images/IPL.png" alt="Logo_IPL_Group"></a>
     </div>
