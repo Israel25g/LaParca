171732ccf0f82e440b44730a07a3714962c7d2ad
alcibiades g <alcibiades@iplgsc.com>
2024-10-29 14:01:29 -0500

    añadiendo estilos

--- daily_plan/operaciones/tabla_multiple.php
+++ daily_plan/operaciones/tabla_multiple.php
@@ -83,7 +83,7 @@ try {
       <p id="hora-actual">prueba</p>
     </div>
   </div>
-  <div class="container  start-0">
+  <div class="container" style="margin-left:-400px">
     <!-- Filtro para la consulta -->
     <h2 class="mt-4 nombre-tabla"><a href="../helpdesk.php"><i class="bi bi-caret-left-fill arrow-back"></i></a>Operaciones</h2>
     <form method="GET" class="mb-3">
@@ -120,11 +120,11 @@ try {
     <!-- Mostrar el mensaje sobre el filtro aplicado -->
     <div class="alert alert-info w-25 p3">
         <?php if ($filtro == 'import'): ?>
-            <p>Mostrando datos de Import.</p>
+            <p class="text-bg-info">Mostrando datos de Import.</p>
         <?php elseif ($filtro == 'export'): ?>
-            <p>Mostrando datos de Export.</p>
+            <p class="text-bg-danger">Mostrando datos de Export.</p>
         <?php elseif ($filtro == 'picking'): ?>
-            <p>Mostrando los datos de Picking.</p>
+            <p class="text-bg-warning">Mostrando los datos de Picking.</p>
         <?php endif; ?>
     </div>
   </div>
