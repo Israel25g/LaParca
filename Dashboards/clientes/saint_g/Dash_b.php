2e54a89cd4ef96547be96ee4d59e67dc908b982f
alcibiades g <alcibiades@iplgsc.com>
2024-11-27 11:23:44 -0500

    fix:titulo

--- Dashboards/clientes/saint_g/Dash_b.php
+++ Dashboards/clientes/saint_g/Dash_b.php
@@ -87,7 +87,7 @@ try {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>IPL Group></title>
+    <title>IPL Group</title>
     <link rel="stylesheet" href="../../../estilos.css">
     <link rel="shortcut icon" href="../../../images/ICO.png">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
