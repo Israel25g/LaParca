2e0b816e43e414d5ca5923e7098241d567bebde8
Israel <israel@iplgsc.com>
2024-12-12 16:33:07 -0500

    Zona horaria arreglada

--- apertura_sesion.php
+++ apertura_sesion.php
@@ -2,6 +2,13 @@
 session_unset();
 session_start();
 date_default_timezone_set('America/Panama');
+?>
+
+<script>
+    console.log('<?php echo date_default_timezone_get();?>')
+</script>
+
+<?php
 
 if (!isset($_SESSION['user'])) {    
     header('Location: index.php?error=No has iniciado sesión.');
