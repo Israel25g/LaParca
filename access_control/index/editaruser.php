83996491326b13868fea1578110b1a20b573cd00
Israel <israel@iplgsc.com>
2024-12-13 15:44:59 -0500

    fecha y hora del servidor actualizada

--- access_control/index/editaruser.php
+++ access_control/index/editaruser.php
@@ -10,7 +10,7 @@ if ($_SERVER["REQUEST_METHOD"] = "POST"){
     $correo = $_POST['email'];
     $departamento = $_POST['departamento'];
 
-    $sql = "UPDATE users SET id='$id', usuario='$nombre', user='$usuario', email='$correo', rol_id='$departamento', updated_at = NOW() WHERE id='$id'";
+    $sql = "UPDATE users SET id='$id', usuario='$nombre', user='$usuario', email='$correo', rol_id='$departamento', updated_at = CONVERT_TZ(NOW(), '+00:00', '-05:00') WHERE id='$id'";
     
     $result = mysqli_query($conexion, $sql);
     if ($result) {
